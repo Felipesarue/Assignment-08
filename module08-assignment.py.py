e8c18ff26bfe428f096201b418ce6db48574456c
Felipesarue <fesarue@gmail.com>
2026-05-06 20:11:32 -0400

    Update module08-assignment.py.py
    # Assignment 8 This repository contains my work for Assignment 8.
    
    ## Files Included
    - main.py
    
    ## What I Practiced
    In this assignment, I practiced working with Python dictionaries and basic data aggregation. I created nested data structures to manage customers, services, and projects, and performed operations such as data lookup, updates, filtering, and summarization. I also practiced using dictionary comprehensions, functions, and basic business logic to analyze service usage, budgets, and customer activity.

--- module08-assignment.py.py
+++ module08-assignment.py.py
@@ -1,4 +1,11 @@
-# Module 8 Assignment: Data Lookup with Dictionaries & Basic Aggregation
+# # Assignment 8 This repository contains my work for Assignment 8.
+
+## Files Included
+#- main.py
+
+## What I Practiced
+#In this assignment, I practiced working with Python dictionaries and basic data aggregation. I created nested data structures to manage customers, services, and projects, and performed operations such as data lookup, updates, filtering, and summarization. I also practiced using dictionary comprehensions, functions, and basic business logic to analyze service usage, budgets, and customer activity.
+
 # GlobalTech Solutions Customer Management System
 
 # Welcome message
